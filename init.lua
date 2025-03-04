f7aedd3cde834ff58b8e3fb07ea3829faf0d204b
Kris Wouk <kris@wouk.me>
2025-03-04 18:05:17 -0500

    added telekasten and lazygit plugins

--- init.lua
+++ init.lua
@@ -448,6 +448,50 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'renerocksai/telekasten.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('telekasten').setup {
+        home = vim.fn.expand '~/Nextcloud/Notes', -- Put the name of your notes directory here
+      }
+      -- Launch panel if nothing is typed after <leader>z
+      vim.keymap.set('n', '<leader>z', '<cmd>Telekasten panel<CR>')
+
+      -- Most used functions
+      vim.keymap.set('n', '<leader>zf', '<cmd>Telekasten find_notes<CR>')
+      vim.keymap.set('n', '<leader>zg', '<cmd>Telekasten search_notes<CR>')
+      vim.keymap.set('n', '<leader>zd', '<cmd>Telekasten goto_today<CR>')
+      vim.keymap.set('n', '<leader>zz', '<cmd>Telekasten follow_link<CR>')
+      vim.keymap.set('n', '<leader>zn', '<cmd>Telekasten new_note<CR>')
+      vim.keymap.set('n', '<leader>zc', '<cmd>Telekasten show_calendar<CR>')
+      vim.keymap.set('n', '<leader>zb', '<cmd>Telekasten show_backlinks<CR>')
+      vim.keymap.set('n', '<leader>zI', '<cmd>Telekasten insert_img_link<CR>')
+
+      -- Call insert link automatically when we start typing a link
+      vim.keymap.set('i', '[[', '<cmd>Telekasten insert_link<CR>')
+    end,
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
